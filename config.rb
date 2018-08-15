e64f954672d7ca3efe97a076f5be5f85631d0726
xselsior <m.tolstov2013@yandex.ru>
2018-08-15 12:24:40 +0300

    Картинка фоном 15 авг

---
+++ config.rb
@@ -0,0 +1,24 @@
+require 'compass/import-once/activate'
+# Require any additional compass plugins here.
+
+http_path = "D:/www sites/novizm"
+css_dir = "dist"
+sass_dir = "sass"
+images_dir = "images"
+generated_images_path = "D:/www sites/novizm/dist/images"
+javascripts_dir = "javascripts"
+fonts_dir = "webfonts"
+
+output_style = :compact
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+line_comments = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass css scss && rm -rf sass && mv scss sass
